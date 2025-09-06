d0dc0f0860d6f5be75ea975c3859d87b6e678bb6
matv864 <matv864@gmail.com>
2025-09-06 12:54:07 +0300

    add modes for correct logging

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from functools import cached_property
 
 from pydantic import HttpUrl
@@ -6,6 +8,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    MODE: Literal["production", "debug"] = "production"
 
     ADMIN_PATH: str
 
--- src/utils/log.py
+++ src/utils/log.py
@@ -1,2 +1,7 @@
 import logging
-logger = logging.getLogger("_granian")
+from src.settings import settings
+
+if settings.MODE == "debug":
+    logger = logging.getLogger("granian.access")
+else:
+    logger = logging.getLogger("_granian")
